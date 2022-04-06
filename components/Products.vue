d540cd8245b28444cfce70471ff40a6b11d1e018
danielputraclt <danielputraclt97@gmail.com>
2022-04-06 11:33:14 +0700

    kasir

---
+++ components/Products.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-row>
+    <v-col v-for="(product, index) in products" cols="2">
+      <v-card :title="product.title"
+      ripple="true">
+        <v-card-action>
+          <v-img :src="require(`@/assets/images/products/${product.thumbnail}`)"></v-img>
+        </v-card-action>
+        <v-card-text align="center" class="product-title">
+            {{ product.title }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default ({
+  data() {
+    return {
+      products: [
+        { id : 1 , title : 'Asus Zenfone', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Canon EOS 700d', thumbnail : 'canon-eos-700d.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},
+        { id : 2 , title : 'Asus', thumbnail : 'asus-zenfone.png', price : 2000000 , categoryID : 1},        
+      ]
+    }
+  }
+})
+</script>
+
+<style scoped>
+ .product-title {
+   white-space: nowrap;
+   overflow: hidden;
+   text-overflow: ellipsis;
+ }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col cols="8" class="frame-content px-6">
+      <products></products>
+    </v-col>
+
+    <v-col cols="4"> PRODUCT </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+    .frame-content {
+      height: 90vh;
+      overflow-y:auto ;
+}
+</style>
