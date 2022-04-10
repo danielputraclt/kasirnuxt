20b06e6027a860adc85a08dda147df0e0e401145
danielputraclt <danielputraclt97@gmail.com>
2022-04-11 00:38:06 +0700

    efek loading

--- components/Products.vue
+++ components/Products.vue
@@ -1,18 +1,29 @@
 <template>
   <v-row>
     <v-row>
-      <v-col cols="10"> Search </v-col>
+      <v-col cols="10">
+        <v-autocomplete
+        label="Products"
+        placeholder="Start typing to search"
+        :search-input.sync="search"
+        :loading="isLoading"
+        ></v-autocomplete>
+      </v-col>
 
       <v-col cols="2">
         <v-menu>
-          <template v-slot:activator="{ on: category }" >
-            <v-btn v-on="category" color="primary" > Category </v-btn>
+          <template v-slot:activator="{ on: category }">
+            <v-btn v-on="category" color="primary"> Category </v-btn>
           </template>
 
           <v-list>
             <v-list-item-group v-model="categoryID">
-              <v-list-item v-for="(category, index) in categories"
-              :value="category.id">
+              <v-list-item
+                v-for="(category, index) in categories"
+                :key="index"
+                :value="category.id"
+                :disabled="category.id == categoryID"
+              >
                 <v-list-item-title>
                   {{ category.title }}
                 </v-list-item-title>
@@ -24,8 +35,8 @@
     </v-row>
 
     <v-row>
-      <v-col v-for="(product, index) in filteredProducts" cols="2">
-        <v-card :title="product.title" ripple="true">
+      <v-col v-for="(product, index) in filteredProducts" cols="2" :key="index">
+        <v-card :title="product.title" ripple=true>
           <v-card-action>
             <v-img
               :src="require(`@/assets/images/products/${product.thumbnail}`)"
@@ -44,7 +55,7 @@
 export default {
   data() {
     return {
-      categoryID:false,
+      categoryID: false,
       categories: [
         {
           id: false,
@@ -170,15 +181,26 @@ export default {
           categoryID: 3,
         },
       ],
+      search: null,
+      isLoading: false,
+      
     }
   },
 
   computed: {
     filteredProducts() {
-      if(this.categoryID){
-        return this.products.filter(s => s.categoryID == this.categoryID)
+      if (this.categoryID) {
+        return this.products.filter((s) => s.categoryID == this.categoryID)
       }
       return this.products
+    },
+  },
+
+  watch: {
+    search(val){
+      console.log(val)
+      this.isLoading = true
+        setTimeout(() => { this.isLoading = false}, 500)
     }
   }
 }
