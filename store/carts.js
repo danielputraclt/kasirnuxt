2227c51cd261673fc37f7d4f852f77388830e113
danielputraclt <danielputraclt97@gmail.com>
2022-04-19 16:27:27 +0700

    update quantity items

--- store/carts.js
+++ store/carts.js
@@ -1,23 +1,30 @@
 export const state = () => ({
-    items:[],
+  items: [],
 })
 
 export const mutations = {
-    addItem(state, id){
-        state.items.push({
-            id:id,
-            quantity : 1
-        })
-
-        console.log(state.items)
-    }
+  addItem(state, id) {
+    state.items.push({
+      id: id,
+      quantity: 1,
+    })
+  },
+  incrementItem(state, id) {
+    state.items.find((item) => item.id === id).quantity++
+  },
 }
 
 export const actions = {
-    addToCart({commit, state}, id){
-        // const productItem = state.items.find(item => item.id === id )
-        // if(!productItem){
-            commit('addItem', id)
-        // }
+  addToCart({ commit, state }, id) {
+    const found = state.items.find((item) => item.id === id)
+    if (found) {
+      commit('incrementItem', id)
+    } else {
+      commit('addItem', id)
     }
+    // const productItem = state.items.find(item => item.id === id )
+    // if(!productItem){
+
+    // }
+  },
 }
